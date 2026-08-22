2f1bdfe5eb9561a85a07fa1c284e6272d489f0ae
malik12 <malik.webd@gmail.com>
2026-08-22 20:29:02 +0200

    structure MVC initiale

---
+++ app/config/params.php
@@ -0,0 +1,8 @@
+<?php
+// params
+
+// parametres de connextion a la db
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'photo_hunter');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
---
+++ core/init.php
@@ -0,0 +1,8 @@
+<?php
+// initialialisation
+
+// 1. charger les params
+require_once '../app/config/params.php';
+
+// 2. charger la connexion
+require_once '../core/connexion.php';
